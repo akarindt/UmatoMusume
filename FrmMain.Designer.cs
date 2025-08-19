b4189422d1674488c9e4409e0d4b61919b5a39ee
akarindt <thang.nd3103@gmail.com>
2025-08-19 21:40:15 +0700

    Add races textbox

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -52,6 +52,7 @@
             groupBox2.SuspendLayout();
             groupBox4.SuspendLayout();
             groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
@@ -61,6 +62,7 @@
             groupBox1.Controls.Add(btnCaptureEvent);
             groupBox1.Dock = DockStyle.Top;
             groupBox1.Location = new Point(0, 0);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new Padding(12, 3, 12, 3);
             groupBox1.Size = new Size(384, 81);
@@ -146,8 +148,7 @@
             // rtbOptions
             // 
             rtbOptions.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            rtbOptions.Location = new Point(6, 70);
-            rtbOptions.Margin = new Padding(3, 2, 3, 2);
+            rtbOptions.Location = new Point(7, 93);
             rtbOptions.Name = "rtbOptions";
             rtbOptions.Size = new Size(413, 216);
             rtbOptions.TabIndex = 6;
@@ -156,10 +157,10 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 43);
+            label4.Location = new Point(7, 57);
             label4.Name = "label4";
-            label4.Padding = new Padding(0, 5, 0, 5);
-            label4.Size = new Size(59, 25);
+            label4.Padding = new Padding(0, 7, 0, 7);
+            label4.Size = new Size(72, 34);
             label4.TabIndex = 4;
             label4.Text = "Char info:";
             // 
@@ -167,7 +168,7 @@
             // 
             lblEventName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             lblEventName.AutoEllipsis = true;
-            lblEventName.Location = new Point(87, 18);
+            lblEventName.Location = new Point(99, 24);
             lblEventName.Name = "lblEventName";
             lblEventName.Padding = new Padding(0, 5, 0, 5);
             lblEventName.Size = new Size(272, 25);
@@ -180,10 +181,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 18);
+            label1.Location = new Point(7, 24);
             label1.Name = "label1";
-            label1.Padding = new Padding(0, 5, 0, 5);
-            label1.Size = new Size(75, 25);
+            label1.Padding = new Padding(0, 7, 0, 7);
+            label1.Size = new Size(93, 34);
             label1.TabIndex = 0;
             label1.Text = "Event name: ";
             // 
@@ -268,8 +269,7 @@
             // rtbObjectives
             // 
             rtbObjectives.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            rtbObjectives.Location = new Point(6, 20);
-            rtbObjectives.Margin = new Padding(3, 2, 3, 2);
+            rtbObjectives.Location = new Point(7, 27);
             rtbObjectives.Name = "rtbObjectives";
             rtbObjectives.Size = new Size(413, 312);
             rtbObjectives.TabIndex = 0;
@@ -277,9 +277,12 @@
             // 
             // FrmMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(384, 791);
+            ClientSize = new Size(437, 1055);
+            Controls.Add(splitter2);
+            Controls.Add(splitter1);
+            Controls.Add(groupBox4);
             Controls.Add(groupBox3);
             Controls.Add(splitterOptions);
             ClientSize = new Size(437, 1055);
@@ -291,6 +294,7 @@
             Controls.Add(groupBox1);
             Controls.Add(groupBox3);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmMain";
             Text = "FrmMain";
             FormClosing += FrmMain_FormClosing;
@@ -301,6 +305,7 @@
             groupBox4.ResumeLayout(false);
             groupBox4.PerformLayout();
             groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
