857e5f59141fbbdc3d6efd79fe87e240274963d3
akarindt <thang.nd3103@gmail.com>
2025-08-20 21:37:31 +0700

    - Complete date/time capture & get races based on date/time. - Need further testing for better result.

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -61,6 +61,7 @@
             groupBox1.Controls.Add(btnCaptureEvent);
             groupBox1.Dock = DockStyle.Top;
             groupBox1.Location = new Point(0, 0);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new Padding(12, 3, 12, 3);
             groupBox1.Size = new Size(384, 81);
@@ -72,10 +73,9 @@
             // 
             // btnCaptureDateTime
             // 
-            btnCaptureDateTime.Location = new Point(194, 22);
-            btnCaptureDateTime.Margin = new Padding(3, 2, 3, 2);
+            btnCaptureDateTime.Location = new Point(222, 29);
             btnCaptureDateTime.Name = "btnCaptureDateTime";
-            btnCaptureDateTime.Size = new Size(180, 24);
+            btnCaptureDateTime.Size = new Size(206, 32);
             btnCaptureDateTime.TabIndex = 3;
             btnCaptureDateTime.Text = "Capture date/time";
             btnCaptureDateTime.UseVisualStyleBackColor = true;
@@ -83,10 +83,9 @@
             // 
             // btnDownloadUmaData
             // 
-            btnDownloadUmaData.Location = new Point(6, 50);
-            btnDownloadUmaData.Margin = new Padding(3, 2, 3, 2);
+            btnDownloadUmaData.Location = new Point(7, 67);
             btnDownloadUmaData.Name = "btnDownloadUmaData";
-            btnDownloadUmaData.Size = new Size(368, 22);
+            btnDownloadUmaData.Size = new Size(421, 29);
             btnDownloadUmaData.TabIndex = 2;
             btnDownloadUmaData.Text = "Download data";
             btnDownloadUmaData.UseVisualStyleBackColor = true;
@@ -94,9 +93,10 @@
             // 
             // btnCaptureEvent
             // 
-            btnCaptureEvent.Location = new Point(6, 22);
+            btnCaptureEvent.Location = new Point(7, 29);
+            btnCaptureEvent.Margin = new Padding(3, 4, 3, 4);
             btnCaptureEvent.Name = "btnCaptureEvent";
-            btnCaptureEvent.Size = new Size(183, 23);
+            btnCaptureEvent.Size = new Size(209, 31);
             btnCaptureEvent.TabIndex = 0;
             btnCaptureEvent.Text = "Capture event";
             btnCaptureEvent.UseVisualStyleBackColor = true;
@@ -137,7 +137,8 @@
             cboCharacterName.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             cboCharacterName.AutoCompleteSource = AutoCompleteSource.ListItems;
             cboCharacterName.FormattingEnabled = true;
-            cboCharacterName.Location = new Point(87, 43);
+            cboCharacterName.Location = new Point(99, 57);
+            cboCharacterName.Margin = new Padding(3, 4, 3, 4);
             cboCharacterName.Name = "cboCharacterName";
             cboCharacterName.Size = new Size(319, 28);
             cboCharacterName.TabIndex = 7;
@@ -146,20 +147,19 @@
             // rtbOptions
             // 
             rtbOptions.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            rtbOptions.Location = new Point(6, 70);
-            rtbOptions.Margin = new Padding(3, 2, 3, 2);
+            rtbOptions.Location = new Point(7, 93);
             rtbOptions.Name = "rtbOptions";
-            rtbOptions.Size = new Size(362, 163);
+            rtbOptions.Size = new Size(413, 216);
             rtbOptions.TabIndex = 6;
             rtbOptions.Text = "";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 43);
+            label4.Location = new Point(7, 57);
             label4.Name = "label4";
-            label4.Padding = new Padding(0, 5, 0, 5);
-            label4.Size = new Size(59, 25);
+            label4.Padding = new Padding(0, 7, 0, 7);
+            label4.Size = new Size(72, 34);
             label4.TabIndex = 4;
             label4.Text = "Char info:";
             // 
@@ -167,7 +167,7 @@
             // 
             lblEventName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             lblEventName.AutoEllipsis = true;
-            lblEventName.Location = new Point(87, 18);
+            lblEventName.Location = new Point(99, 24);
             lblEventName.Name = "lblEventName";
             lblEventName.Padding = new Padding(0, 5, 0, 5);
             lblEventName.Size = new Size(272, 25);
@@ -180,10 +180,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 18);
+            label1.Location = new Point(7, 24);
             label1.Name = "label1";
-            label1.Padding = new Padding(0, 5, 0, 5);
-            label1.Size = new Size(75, 25);
+            label1.Padding = new Padding(0, 7, 0, 7);
+            label1.Size = new Size(93, 34);
             label1.TabIndex = 0;
             label1.Text = "Event name: ";
             // 
@@ -268,8 +268,7 @@
             // rtbObjectives
             // 
             rtbObjectives.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            rtbObjectives.Location = new Point(6, 20);
-            rtbObjectives.Margin = new Padding(3, 2, 3, 2);
+            rtbObjectives.Location = new Point(7, 27);
             rtbObjectives.Name = "rtbObjectives";
             rtbObjectives.Size = new Size(413, 312);
             rtbObjectives.TabIndex = 0;
@@ -277,7 +276,7 @@
             // 
             // FrmMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(382, 791);
             Controls.Add(groupBox4);
@@ -297,6 +296,7 @@
             Controls.Add(groupBox1);
             Controls.Add(groupBox3);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmMain";
             Text = "FrmMain";
             FormClosing += FrmMain_FormClosing;
--- FrmMain.cs
+++ FrmMain.cs
@@ -362,6 +362,7 @@ namespace UmatoMusume
                         var careerOptions = _careerList.GetCareerEvents(lblEventName.Text);
                         if (careerOptions.Any())
                         {
+       
                             foreach (var option in careerOptions.SelectMany(x => x))
                             {
                                 if (!string.IsNullOrEmpty(option.Key))
@@ -384,11 +385,11 @@ namespace UmatoMusume
                 {
                     foreach (var race in races)
                     {
-                        rtbOptions.SelectionFont = new Font(rtbOptions.Font, FontStyle.Bold);
-                        rtbOptions.AppendText($"{race.RaceName}({race.Grade}) - {race.Terrain} - {race.DistanceType} - {race.DistanceMeter}\n");
+                        rtbRaces.SelectionFont = new Font(rtbRaces.Font, FontStyle.Bold);
+                        rtbRaces.AppendText($"{race.RaceName}({race.Grade}) - {race.Terrain} - {race.DistanceType} - {race.DistanceMeter}\n");
 
-                        rtbOptions.SelectionFont = new Font(rtbOptions.Font, FontStyle.Regular);
-                        rtbOptions.AppendText($"Fans required: {race.FansRequired} - Gained: {race.FansGained}" + "\n---------------\n");
+                        rtbRaces.SelectionFont = new Font(rtbRaces.Font, FontStyle.Regular);
+                        rtbRaces.AppendText($"Fans required: {race.FansRequired} - Gained: {race.FansGained}" + "\n---------------\n");
                     }
                 }
             }
